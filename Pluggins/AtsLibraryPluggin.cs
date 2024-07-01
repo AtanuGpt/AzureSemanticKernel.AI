29d2cc84d6b9b1ad14427f22fe141eec3940ff6f
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2024-07-01 17:31:23 +0530

    Add files via upload

---
+++ Pluggins/AtsLibraryPluggin.cs
@@ -0,0 +1,28 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace AzureSemanticKernel.AI.Pluggins
+{
+
+    public class AtsLibraryPluggin
+    {
+        [KernelFunction, Description("Gets the content of the CV")]
+        public static string GeResumeDetails()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "\\docs\\cv.pdf";
+            PdfReader reader = new PdfReader(path);
+
+            string text = string.Empty;
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(reader, page);
+            }
+            reader.Close();
+
+            return text;
+        }
+    }
+}
