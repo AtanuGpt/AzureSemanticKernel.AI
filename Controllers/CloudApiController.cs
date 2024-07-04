d415b83d01f3e9535929fd71d3116988d32bd138
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2024-07-04 13:11:28 +0530

    Add files via upload

---
+++ Controllers/CloudApiController.cs
@@ -0,0 +1,40 @@
+﻿using AzureSemanticKernel.AI.Pluggins;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Planning.Handlebars;
+
+namespace AzureSemanticKernel.AI.Controllers
+{
+    public class CloudApiController : Controller
+    {
+        public async Task<string> GetResponse(string userQuestion)
+        {
+            var kernel = Common.initializeKernel();
+
+            var cloudPlugin = kernel.ImportPluginFromType<CloudApiLibraryPluggin>();
+
+            var arguments = new KernelArguments();
+
+            arguments["input"] = userQuestion;
+
+            OpenAIPromptExecutionSettings openAiPromptExecutionSetting = new()
+            {
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
+
+            #pragma warning disable SKEXP0060 
+            var planner = new HandlebarsPlanner();
+
+            var originalPlan = await planner.CreatePlanAsync(kernel, userQuestion);
+
+            //Console.WriteLine(originalPlan);
+
+            var originalPlanResult = await originalPlan.InvokeAsync(kernel, new KernelArguments(openAiPromptExecutionSetting));
+            
+            #pragma warning restore SKEXP0060
+
+            return originalPlanResult.ToString();
+        }
+    }
+}
